56bf0196d5605e214e16086afcc7cfc90176714c
Harshit Kumar Prajapat <harshit.kumar3805@gmail.com>
2026-06-18 21:40:35 +0530

    main file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import requests
 from config import API_KEY
+from weather_utils import weather_report
 
-city = "Jaipur"
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+def get_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    return response.json()
 
-response = requests.get(url)
 
-data = response.json()
 
-print(data)
+city = input('Enter the city: ')
+data = get_weather(city)
+
+if data['cod'] == 200:
+    weather_report(data)
+else:
+    print('City not found!')
